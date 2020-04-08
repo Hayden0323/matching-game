61056c52d83d861c2912572753aef694f420adfa
Hayden <hayden@HaydendeMacBook-Pro.local>
2020-04-08 17:28:55 +0800

    commit

---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="buttonArea">
+		<button type="default" :style="{margin, fontSize, width, height}" @click="$emit('card-click')">{{isShow ? title : cover}}</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			margin: {
+				type: String,
+				default : '0px'
+			},
+			fontSize: {
+				type: String,
+				default: '24px'
+			},
+			title: {
+				type: String,
+				default: ''
+			},
+			cover: {
+				type: String,
+				default: ''
+			},
+			isShow: {
+				type: Boolean,
+				default: false
+			},
+			width: {
+				type: String,
+				default: '48px'
+			},
+			height: {
+				type: String,
+				default: '48px'
+			}
+		},
+		methods: {
+			
+		},
+	}
+</script>
+
+<style>
+	.buttonArea button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,186 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+	<view class="content" :style="{minHeight: windowHeight + 'px'}">
+		<view class="header">
+			<span> Matching Game </span>
+		</view>
+		<view class="main">
+			<view class="gameBoard">
+				<Card v-for="(item, index) in randomSymbols" 
+							:key="index" 
+							:margin="((windowWidth - (48 * 4)) / (5 * 2)) + 'px'" 
+							width="48px"
+							height="48px"
+							fontSize="36px" 
+							:title="item" 
+							cover="❓" 
+							:isShow="isOpen[index]"
+							@card-click="cardClickHandler(index)">
+				</Card>
+			</view>
+		</view>
+		<view class="footer">
+			<span> {{
+				isEnded
+					? `Congrats! You have completed in ${steps} steps.`
+					: `You have tried ${steps} times.`
+			}} </span>
+			<Card v-if="isEnded"
+						margin="5px"
+						width="120px"
+						height="25px"
+						fontSize="15px" 
+						title="Try Again" 
+						cover="❓" 
+						:isShow=true
+						@card-click="initGame">
+			</Card>
 		</view>
 	</view>
 </template>
 
 <script>
+	import Card from '../../components/Card.vue'
 	export default {
+		components: {
+			Card,
+		},
 		data() {
 			return {
-				title: 'Hello'
+				windowWidth: 0,
+				windowHeight: 0,
+				cardSymbols: ["🐱", "🐽", "🎃", "🌎", "👗", "👠", "👕" ,"⚽️"],
+				randomSymbols: [],
+				isOpen: [],
+				firstPickIndex: null,
+				secondPickIndex: null,
+				steps: 0,
+				isEnded: false
 			}
 		},
 		onLoad() {
-
+			this.getWindowInfo();
+			this.getRandomSymbols()
 		},
 		methods: {
-
+			getWindowInfo () {
+				const { windowWidth, windowHeight } = uni.getSystemInfoSync();
+				this.windowHeight = windowHeight
+				this.windowWidth = windowWidth
+			},
+			
+			getRandomSymbols () {
+				let newArray = [...this.cardSymbols, ...this.cardSymbols];
+				let len = newArray.length;
+				for (let i = 0; i < len; i++) {
+					let randomIndex = Math.floor(Math.random() * len)
+					let tmp = newArray[i];
+					newArray[i] = newArray[randomIndex];
+					newArray[randomIndex] = tmp;
+				}
+				for (let i = 0; i < newArray.length; i++) {
+					this.isOpen.push(false);
+				}
+				this.randomSymbols = newArray;
+			},
+			
+			cardClickHandler (index) {
+				let isOpen = [...this.isOpen];
+				
+				if (isOpen[index] == true) return;
+				if (this.firstPickIndex !== null && this.secondPickIndex !== null) return;
+				
+				isOpen[index] = true;
+				if (this.firstPickIndex == null && this.secondPickIndex == null) {
+					this.isOpen = isOpen;
+					this.firstPickIndex = index;
+				} else if (this.firstPickIndex !== null && this.secondPickIndex == null) {
+					this.isOpen = isOpen;
+					this.secondPickIndex = index;
+				}
+				
+				this.steps++;
+				this.calculateGameResult();
+			},
+			
+			calculateGameResult () {
+				if (this.firstPickIndex !== null && this.secondPickIndex !== null) {
+					
+					if (this.isOpen.filter(v => v == false).length == 0) {
+						this.isEnded = true;
+					}
+					
+					let firstSymbol = this.randomSymbols[this.firstPickIndex];
+					let secondSymbol = this.randomSymbols[this.secondPickIndex];
+					
+					if (firstSymbol !== secondSymbol) {
+						// inCorrect
+						let isOpen = [...this.isOpen];
+						setTimeout(() => {
+							isOpen[this.firstPickIndex] = false;
+							isOpen[this.secondPickIndex] = false;
+							this.isOpen = isOpen;
+							this.firstPickIndex = null;
+							this.secondPickIndex = null;
+						}, 500)
+					} else {
+						// correct
+						this.firstPickIndex = null;
+						this.secondPickIndex = null;
+					}
+				}
+			},
+			
+			initGame () {
+				this.isOpen = [];
+				this.steps = 0;
+				this.isEnded = false;
+				this.getRandomSymbols();
+			}
 		}
 	}
 </script>
 
 <style>
 	.content {
-		text-align: center;
-		height: 400upx;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		background-color: yellow;
+	}
+	.header {
+		display: flex;
+		flex:1;
+		justify-content: center;
+		align-items: center;
+		background-color: #eee;
+	}
+	.header span {
+		font-weight: bold;
+		font-size: 36px;
+	}
+	.main {
+		display: flex;
+		flex:3;
+		background-color: #fff;
+	}
+	.footer {
+		display: flex;
+		flex:1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: #eee;
+	}
+	.footer span {
+		font-size: 15px;
+	}
+	.gameBoard {
+		display: flex;
+		flex: 1;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-content: center;
 	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
